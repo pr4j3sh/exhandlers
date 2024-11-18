c4110a80bf80db0d8d367a3798c7fb0650c6a139
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-18 21:35:00 +0530

    added morgan

--- src/cors.handler.js
+++ src/cors.handler.js
@@ -1,20 +1,19 @@
 const cors = require("cors");
 
-const options = {
-  origin: (origin, callback) => {
-    const allowedOrigins = process.env.ALLOWED_ORIGINS
-      ? process.env.ALLOWED_ORIGINS.split(",")
-      : [];
+const corsHandler = (allowedOrigins) => {
+  const options = {
+    origin: (origin, callback) => {
+      const allowedOrigins = process.env.ALLOWED_ORIGINS
+        ? process.env.ALLOWED_ORIGINS.split(",")
+        : [];
 
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Origin not allowed"));
-    }
-  },
-};
-
-const corsHandler = () => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Origin not allowed"));
+      }
+    },
+  };
   return cors(options);
 };
 
