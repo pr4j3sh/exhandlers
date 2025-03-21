f81d4f937a36687220957a94baef87480dd4197a
pr4j3sh <prajesh.eleven118@gmail.com>
2025-03-21 21:43:30 +0530

    fix: promise error in tokenHandler

--- src/token.handler.js
+++ src/token.handler.js
@@ -2,25 +2,30 @@ const jwt = require("jsonwebtoken");
 
 /**
  * @function tokenHandler
- * @description Generates a JSON Web Token (JWT).
+ * @description Asynchronously generates a JSON Web Token (JWT).
  *   See the official jsonwebtoken package documentation for more details:
  *   @see {@link https://www.npmjs.com/package/jsonwebtoken}
  *
  * @param {Object} payload - The payload to encode in the token.
  * @param {string} secret - The secret key used to sign the token.
  * @param {Object} [args={}] - Additional options for the JWT (e.g., expiresIn).
- * @returns {string} The generated JWT.
+ * @returns {Promise<string>} A promise that resolves to the generated JWT.
  *
  * @example
  * ```js
  * const { tokenHandler } = require("./tokenHandler");
- * const token = tokenHandler({ userId: 123 }, "mySecret", { expiresIn: "1h" });
- * console.log(token);
+ * tokenHandler({ userId: 123 }, "mySecret", { expiresIn: "1h" })
+ *   .then(token => console.log(token))
+ *   .catch(err => console.error(err));
  * ```
  */
-const tokenHandler = (payload = {}, secret, args = {}) => {
-  const token = jwt.sign(payload, secret, args);
-  return token;
+const tokenHandler = async (payload = {}, secret, args = {}) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, secret, args, (token, err) => {
+      if (err) reject(err);
+      else resolve(token);
+    });
+  });
 };
 
 module.exports = { tokenHandler };
